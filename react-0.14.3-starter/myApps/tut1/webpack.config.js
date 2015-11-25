74fa84aa1953e384fd9bc065f00ece0f2ae56eb0
bpshonyak <bpshonyak@live.com>
2015-11-25 07:10:29 -0800

    added bable loader

--- react-0.14.3-starter/myApps/tut1/webpack.config.js
+++ react-0.14.3-starter/myApps/tut1/webpack.config.js
@@ -6,4 +6,14 @@ module.exports = {
     filename: "bundle.js",
     path: __dirname + "/dist",
   },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  }
 };
